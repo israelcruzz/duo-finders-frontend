c5a94e19f4ebb2b3821786ee6b0679e9743e61ae
israelcruzz <israelcruz.contato@gmail.com>
2024-04-27 16:41:38 -0300

    feat: search input component was created

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { SearchInput } from "@/components/search-input";
 import { Metadata } from "next";
 import { FiSearch } from "react-icons/fi";
 
@@ -8,14 +9,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main className="w-full h-full px-6 py-4 xl:pl-80 xl:py-4">
-      <div className="w-full h-[52px] p-6 bg-black text-[#7B7B7B] flex rounded-lg justify-between items-center gap-2">
-        <input
-          type="text"
-          placeholder="Search Games..."
-          className="bg-transparent outline-none flex-1"
-        />
-        <FiSearch size={24} />
-      </div>
+      <SearchInput />
     </main>
   );
 }
---
+++ src/components/search-input/index.tsx
@@ -0,0 +1,14 @@
+import { FiSearch } from "react-icons/fi";
+
+export function SearchInput() {
+  return (
+    <div className="w-full h-[52px] p-6 bg-black text-[#7B7B7B] flex rounded-lg justify-between items-center gap-2">
+      <input
+        type="text"
+        placeholder="Search Games..."
+        className="bg-transparent outline-none flex-1"
+      />
+      <FiSearch size={24} />
+    </div>
+  );
+}
