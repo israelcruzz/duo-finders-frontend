27c17be9a220cce6b6981e7a94043ba62cc0972f
israelcruzz <israelcruz.contato@gmail.com>
2024-04-27 16:39:23 -0300

    refactor: refactoring in menu responsiveness logic

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -15,59 +15,79 @@ export default function RoutesLayout({
   const [menuActive, setMenuActive] = useState<boolean>(true);
 
   return (
-    <main className={`flex ${menuActive ? '' : 'px-6 py-6'} justify-between`}>
-      <button
-        className={`xl:hidden text-white ${menuActive && "hidden"}`}
-        onClick={() => setMenuActive(true)}
-      >
-        <RxDragHandleHorizontal size={32} />
-      </button>
-      <div
-        className={`h-[100vh] w-72 bg-[#121212] flex flex-col p-6 justify-between border-r border-r-[#EFEFEF]/10 ${
-          menuActive ? "" : "hidden"
-        }`}
-      >
-        <section className="flex flex-col gap-6">
-          <div className="flex gap-2 items-center justify-between">
-            <div className="flex items-center gap-2">
-              <Image
-                src="/logo.svg"
-                width={40}
-                height={40}
-                alt="logo duofinders"
-              />
-              <h1 className="text-white text-base font-semibold">Duofinders</h1>
+    <main
+      className={`flex ${menuActive ? "mr-6" : "px-6 py-6"} justify-between`}
+    >
+      <div className="relative flex">
+        <button
+          className={`xl:hidden text-white ${menuActive && "hidden"}`}
+          onClick={() => setMenuActive(true)}
+        >
+          <RxDragHandleHorizontal size={32} />
+        </button>
+
+        <div
+          className={`absolute h-[100vh] min-w-72 bg-[#121212] flex flex-col p-6 justify-between border-r border-r-[#EFEFEF]/10 ${
+            menuActive ? "" : "hidden"
+          }`}
+        >
+          <section className="flex flex-col gap-6 flex-1">
+            <div className="flex gap-2 items-center justify-between">
+              <div className="flex items-center gap-2">
+                <Image
+                  src="/logo.svg"
+                  width={40}
+                  height={40}
+                  alt="logo duofinders"
+                />
+                <h1 className="text-white text-base font-semibold">
+                  Duofinders
+                </h1>
+              </div>
+
+              <button
+                className="xl:hidden text-white"
+                onClick={() => setMenuActive(false)}
+              >
+                <IoCloseOutline size={24} />
+              </button>
             </div>
 
-            <button className="xl:hidden text-white" onClick={() => setMenuActive(false)}>
-              <IoCloseOutline size={24} />
+            <div className="flex flex-col gap-3">
+              <Link href="/">
+                <nav className="p-4 text-white flex gap-2 font-medium bg-[#650C71] rounded-lg hover:bg-[#650C71]/90">
+                  Home
+                </nav>
+              </Link>
+
+              <Link href="/games">
+                <nav className="p-4 text-white flex gap-2 font-medium rounded-lg hover:bg-black/20">
+                  Games
+                </nav>
+              </Link>
+            </div>
+          </section>
+          <section>
+            <button className="text-white flex gap-3">
+              <HiOutlineLogout size={24} />
+              <h1 className="text-base">Logout</h1>
             </button>
-          </div>
-
-          <div className="flex flex-col gap-3">
-            <Link href="/">
-              <nav className="p-4 text-white flex gap-2 font-medium bg-[#650C71] rounded-lg hover:bg-[#650C71]/90">
-                Home
-              </nav>
-            </Link>
-
-            <Link href="/games">
-              <nav className="p-4 text-white flex gap-2 font-medium rounded-lg hover:bg-black/20">
-                Games
-              </nav>
-            </Link>
-          </div>
-        </section>
-        <section>
-          <button className="text-white flex gap-3">
-            <HiOutlineLogout size={24} />
-            <h1 className="text-base">Logout</h1>
-          </button>
-        </section>
+          </section>
+        </div>
       </div>
+
       {children}
-      <section>
-        <Image src="/avatar.svg" width={56} height={56} alt="profile image" className="rounded-full" />
+
+      <section className="min-w-24 flex items-center justify-center py-4 xl:py-4">
+        <Link href="/profile">
+          <Image
+            src="/avatar.svg"
+            width={56}
+            height={56}
+            alt="profile image"
+            className="rounded-full"
+          />
+        </Link>
       </section>
     </main>
   );
--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,9 +1,21 @@
 import { Metadata } from "next";
+import { FiSearch } from "react-icons/fi";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
 export default function Home() {
-  return <h1>a</h1>;
+  return (
+    <main className="w-full h-full px-6 py-4 xl:pl-80 xl:py-4">
+      <div className="w-full h-[52px] p-6 bg-black text-[#7B7B7B] flex rounded-lg justify-between items-center gap-2">
+        <input
+          type="text"
+          placeholder="Search Games..."
+          className="bg-transparent outline-none flex-1"
+        />
+        <FiSearch size={24} />
+      </div>
+    </main>
+  );
 }
