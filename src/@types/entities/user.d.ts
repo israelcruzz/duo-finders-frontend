ee7f3f92832de6da9abe064743da5dfefeb3a67a
israelcruzz <israelcruz.contato@gmail.com>
2024-05-14 19:59:41 -0300

    feat: add entitie category type

---
+++ src/@types/entities/user.d.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  id?: string;
+  name: string;
+  avatar: string;
+  banner: string;
+  discord: string;
+}
--- src/context/auth-provider.tsx
+++ src/context/auth-provider.tsx
@@ -1,14 +1,23 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
-import { ReactNode, useContext } from "react";
+import { ReactNode, useContext, useState } from "react";
 import { AuthContext } from "./auth-context";
+import { signIn, useSession } from 'next-auth/react'
+import { IUser } from "@/@types/entities/user";
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
 export async function AuthProvider({ children }: AuthProviderProps) {
-  const auth = async () => {};
+  const [user, setUser] = useState<IUser>();
+
+  const auth = async () => {
+    signIn();
+
+    const { data } = useSession();
+  };
 
   return (
     <AuthContext.Provider value={{ auth }}>{children}</AuthContext.Provider>
