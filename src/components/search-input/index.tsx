15eaf28e2b9c01f1280bb262587150e17ed4aed5
israelcruzz <israelcruz.contato@gmail.com>
2024-05-27 00:31:27 -0300

    feat: add logic in search input component

--- src/components/search-input/index.tsx
+++ src/components/search-input/index.tsx
@@ -1,14 +1,39 @@
+"use client";
+
+import { redirect, useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { toast } from "sonner";
 
 export function SearchInput() {
+  const [query, setQuery] = useState("");
+
+  const router = useRouter()
+
+  const handleSubmitForm = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!query) {
+      toast.error("Campo Vazio");
+      return;
+    }
+
+    router.push(`http://localhost:3000/search?q=${String(query)}`);
+  };
+
   return (
-    <div className="w-full h-[52px] p-6 bg-black text-[#7B7B7B] flex rounded-lg justify-between items-center gap-2 cursor-pointer">
-      <input
-        type="text"
-        placeholder="Search Games..."
-        className="bg-transparent outline-none flex-1"
-      />
-      <FiSearch size={24} />
-    </div>
+    <form onSubmit={handleSubmitForm}>
+      <div className="w-full h-[52px] p-6 bg-black text-[#7B7B7B] flex rounded-lg justify-between items-center gap-2 cursor-pointer">
+        <input
+          name="q"
+          type="text"
+          placeholder="Pesquise Jogos..."
+          className="bg-transparent outline-none flex-1"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+        <FiSearch size={24} />
+      </div>
+    </form>
   );
 }
